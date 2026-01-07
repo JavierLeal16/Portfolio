97d9dc35429c9040dae4efc0d64d02f8b39ffb2b
Javier Leal <javieralealg@gmail.com>
2026-01-06 20:53:53 -0500

    Created basic AboutMe component. Added styles to the new component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Article from './components/Article.jsx';
 import Header from './components/Header.jsx';
 import NavBar from './components/NavBar.jsx';
 import './App.css';
+import AboutMe from './components/AboutMe.jsx';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <NavBar />
       <main>
+        <AboutMe />
         {Array.from({ length: 20 }).map((_, i) => (
           <Article key={i} />
         ))}
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function AboutMe() {
+  return (
+    <section id='aboutMe'>
+      <h2>About Me</h2>
+      <p>
+        Hello! I'm Javier Leal, a passionate Full Stack Developer with a knack
+        for creating dynamic and responsive web applications. With a strong
+        foundation in both front-end and back-end technologies, I thrive on
+        transforming ideas into functional digital experiences. My expertise
+        includes JavaScript, React, Node.js, and databases like MongoDB and SQL.
+        I enjoy learning about new technologies and strive to continually hone
+        my skills as a developer and engineer. Whenever I'm not coding, I love
+        exploring new tech trends, learning new design patterns and programming
+        languages. <strong>Let's build something amazing together!</strong>
+      </p>
+    </section>
+  );
+}
+export default AboutMe;
