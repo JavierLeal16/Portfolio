eaea72533cb0f78d322546dad0f38aa2dd82523e
Javier Leal <javieralealg@gmail.com>
2026-01-07 21:59:20 -0500

    Modified about me paragraph. Slightly modified the color of the bolded section of the about me paragraph.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,15 +5,14 @@ function AboutMe() {
     <section id='aboutMe'>
       <h2>About Me</h2>
       <p>
-        Hello! I'm Javier Leal, a passionate Full Stack Developer with a knack
-        for creating dynamic and responsive web applications. With a strong
+        Hello! I'm Javier Leal, a passionate Web Developer with a knack for
+        creating dynamic and responsive web applications. With a strong
         foundation in both front-end and back-end technologies, I thrive on
-        transforming ideas into functional digital experiences. My expertise
-        includes JavaScript, React, Node.js, and databases like MongoDB and SQL.
-        I enjoy learning about new technologies and strive to continually hone
-        my skills as a developer and engineer. Whenever I'm not coding, I love
-        exploring new tech trends, learning new design patterns and programming
-        languages. <strong>Let's build something amazing together!</strong>
+        transforming ideas into functional digital experiences. I as a
+        professional, I strive for excellence and to deliver well-built and
+        efficient solutions. Whenever I'm not coding, I love exploring new tech
+        trends, learning new design patterns and programming languages.{' '}
+        <strong>Let's build something amazing together!</strong>
       </p>
     </section>
   );
